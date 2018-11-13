ac6d7ecd708a18254c009256d3f34656f5ba33fc
amorhe <2053734075@qq.com>
2018-11-13 17:53:38 +0800

    11-13 18:00

--- sea/js/public.js
+++ sea/js/public.js
@@ -13,7 +13,8 @@ $('.backdrop').click(function () {
 $('.head_right button').click(function () {
     $('.call').show(200)
 })
-$('.closeX').click(function () {
+//关闭弹框
+$('.guanbi').click(function () {
     $('.call').hide(200)
     $('.setout').hide()
     $('.maichu').hide()
@@ -21,9 +22,6 @@ $('.closeX').click(function () {
 $('.zhuanrang').click(function () {
     $('.setout').show()
 })
-function maichu() {
-    $('.maichu').show()
-}
 $(document).on('click','.showBtn',function () {
     $('.mairu').show();
     $(this).removeClass('sanjiao showBtn')
@@ -44,3 +42,56 @@ $(document).on('click','.maichuHide',function () {
     $(this).removeClass('dsanjiao maichuHide')
     $(this).addClass('sanjiao maichuShow')
 })
+
+
+// function ajaxDemo(page,pageSize){
+//     if(!pageSize)var pageSize = 5;
+//     var data = {},arr=[];
+//     for(var i = 1;i<=31;i++){
+//         arr.push(i);
+//         if(i==pageSize){
+//             data[1]=arr;
+//             arr=[];
+//         }else if(i==pageSize*2){
+//             data[2]=arr;
+//             arr=[];
+//         }else if(i==pageSize*3){
+//             data[3]=arr;
+//             arr=[];
+//         }else if(i==pageSize*4){
+//             data[4]=arr;
+//             arr=[];
+//         }else if(i==pageSize*5){
+//             data[5]=arr;
+//             arr=[];
+//         }else if(i==pageSize*6){
+//             data[6]=arr;
+//             arr=[];
+//         }else if(i>=31){
+//             data[7]=arr;
+//             arr=[];
+//         }
+//     }
+//     // return data[page];
+//     console.log(data[page])
+// }
+//
+// var page = 1;
+// //下一页
+// function next(pageSize) {
+//     if(page>pageSize){
+//         return
+//     }else{
+//         ajaxDemo(page)
+//         page ++;
+//     }
+// }
+// //上一页
+// function prev() {
+//     if(page==1){
+//         return
+//     }else{
+//         page --;
+//         ajaxDemo(page)
+//     }
+// }
